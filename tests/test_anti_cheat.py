4dcae0b1597d8b60a64c4d56745757827de3f1a6
DogeFood Lab Deploy <deploy@dogefoodlab.com>
2026-02-15 10:59:51 +0000

    auto-commit for f59b77eb-8aec-47a7-ae01-98354146d98e

--- tests/test_anti_cheat.py
+++ tests/test_anti_cheat.py
@@ -7,7 +7,7 @@ import pytest
 import requests
 import os
 
-BASE_URL = os.environ.get('REACT_APP_BACKEND_URL', 'https://dogenews-mixer.preview.emergentagent.com')
+BASE_URL = os.environ.get('REACT_APP_BACKEND_URL', 'https://dogemixbot.preview.emergentagent.com')
 
 # Test address for anti-cheat testing
 TEST_ADDRESS = "0xTEST_anticheat_1234567890abcdef12345678"
